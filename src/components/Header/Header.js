e185b9ed0ec2696ceeb2789a281a978418c89fe0
Asaduzzaman <ajaman375@gmail.com>
2021-09-25 14:43:45 -0700

    text alignment Correction

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,17 +5,18 @@ const Header = () => {
     return (
         <div>
             <nav class="navbar navbar-dark bg-dark text-light">
-         <div className="container d-block">
+         <div className="container d-block text-center">
              <article className="title">
              <h1>Make a Tech Team For Future</h1>
              </article>
              <article className="description">
                 <p>we Can Do, You can select us from our panel</p>
              </article>
-        </div>
-        <div className="total-budget">
+             <div className="total-budget">
             <h2>Total Budget : 100 Million</h2>
         </div>
+        </div>
+        
         </nav>
         </div>
     );
