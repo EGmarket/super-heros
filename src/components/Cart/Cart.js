c532d76c4c4d00ade5f64a660313d1de934393c1
Asaduzzaman <ajaman375@gmail.com>
2021-09-25 14:29:53 -0700

    Calculation done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faUserShield } from '@fortawesome/free-solid-svg-icons';
+import './Cart.css';
 
 const Cart = (props) => {
     console.log(props)
     const {cart} = props;
+    //counting total Salary
+    let total = 0;
+    for(const member of cart){
+        total = total + member.salary
+    }
     return (
         <div>
-            <h2>Booking Summary</h2>
-            <h4>Total Member : {cart.length} <FontAwesomeIcon icon={faShoppingCart} /></h4>
+         <div className="details">
+            <h2 className="text-cart">Booking Summary</h2>
+            <h4> <span className="text-cart"> <FontAwesomeIcon icon={faUserShield} /> Total Member :</span>  {cart.length} </h4>
+            <ul>
+                {
+                    cart.map(member=> <li>{member.name} and salary: ${member.salary}</li>)
+                }
+            </ul>
+            <h4> <span className="text-cart"> Your Grand Total:</span> ${total}</h4>
         </div>
+        <div>
+            <button class="btn btn-success custom-btn">PAY NOW</button>
+        </div>
+        </div>
+       
+       
     );
 };
 
